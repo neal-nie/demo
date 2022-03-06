176be355dc7cde79785d25277de46fbf99cc435b
Neal Nie <62248345+neal-nie@users.noreply.github.com>
2022-03-06 19:25:57 +0800

    test stl

---
+++ src/exercise/string_test.cpp
@@ -0,0 +1,21 @@
+/* demo for string in stl of cpp */
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s0;
+    string s1;
+
+    s0 = "demo string";
+    s1 = s0;
+
+    cout << s0 << endl;
+    cout << s1 << endl;
+
+    
+    return 0;
+}
