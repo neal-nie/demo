faac42fda4177b21cbb0b168711d439f64ac07a7
Neal Nie <62248345+neal-nie@users.noreply.github.com>
2022-06-05 12:27:25 +0800

    init work space for cpp

---
+++ lib/box.cpp
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2022-2022 NealNie. All rights reserved.
+ * Description: demo for cpp leanring
+ */
+
---
+++ lib/box.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022-2022 NealNie. All rights reserved.
+ * Description: demo for cpp leanring
+ */
+
+#ifndef BOX_H
+#define BOX_H
+
+#include <stdint.h>
+
+class Box
+{
+};
+
+#endif // !BOX_H
